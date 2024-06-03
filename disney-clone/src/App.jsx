d1caf2133c5316de1290bb8a9818522a7448b710
Nipuni Vithana <ndhananja99@gmail.com>
2024-06-03 20:15:46 +0530

    first commit

---
+++ disney-clone/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Componets/Header'
+import Slider from './Componets/Slider'
+import ProductionHouse from './Componets/ProductionHouse'
+import GenreMovieList from './Componets/GenreMovieList'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <Header/>
+        
+        <Slider/>
+
+        <ProductionHouse/>
+
+        <GenreMovieList/>
+    </div>
+  )
+}
+
+export default App
+
